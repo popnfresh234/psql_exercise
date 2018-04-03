e5fb4eb525ccafeae4b828077116a7cc77642750
Alex Holliday <ajhollid@gmail.com>
2018-04-03 19:37:48 +0000

    Implements knex scripts

---
+++ knex-add-person.js
@@ -0,0 +1,46 @@
+
+const settings = require('./settings');
+
+const knex = require('knex')({
+  client: 'pg',
+  version: '7.2',
+  connection: {
+    host: settings.hostname,
+    user: settings.user,
+    password: settings.password,
+    database: settings.database
+  }
+});
+
+
+let firstName = process.argv.slice(2)[0];
+let lastName = process.argv.slice(2)[1];
+let dob = process.argv.slice(2)[2];
+
+function insertOne(first_name, last_name, dob){
+  knex('famous_people')
+  .insert({
+    first_name: first_name,
+    last_name: last_name,
+    birthdate: dob
+  }).asCallback((err)=>{
+    if ( err ) {
+      console.log("Insert error", error);
+    } else {
+      console.log("Success");
+      knex.destroy();
+    }
+  });
+}
+
+insertOne(firstName, lastName, dob);
+
+// knex.().from('famous_people')
+//   .asCallback((err, rows)=> {
+//     if ( err ){
+//       console.log("Query error", err);
+//     } else {
+//       console.log("Insert success");
+//       knex.destroy();
+//     }
+//   });
---
+++ knex-people-script.js
@@ -0,0 +1,38 @@
+
+const settings = require('./settings');
+
+const knex = require('knex')({
+  client: 'pg',
+  version: '7.2',
+  connection: {
+    host: settings.hostname,
+    user: settings.user,
+    password: settings.password,
+    database: settings.database
+  }
+});
+
+
+let input = process.argv.slice(2)[0];
+
+function logResults(rows){
+  rows.forEach((record, index) => {
+    let indexOffset = index + 1;
+    let humanString = indexOffset + ": " + record.first_name + " " + record.last_name + ", born " + "'" + record.birthdate + "'";
+    console.log(humanString);
+  });
+}
+
+function queryAllByName(input){
+  knex.select().from('famous_people')
+    .asCallback((err, rows)=> {
+      if ( err ){
+        console.log("Query error", err);
+      } else {
+        logResults(rows);
+        knex.destroy();
+      }
+    });
+}
+
+queryAllByName(input);
